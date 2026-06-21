fa849437ea0bdcc6427110d6f639d3d8a0871436
Stayhappy-web <igboanugoemmanuelchidera@gmail.com>
2026-06-21 16:07:40 -0700

    NEW UPDATES

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,56 @@
+import { useState } from "react";
 import logo from "../assets/oak-vine.png";
+
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
+
       <div className="logo-container">
-        <img src={logo} alt="Oak & Vine Logo" className="logo" />
+        <img
+          src={logo}
+          alt="Oak & Vine"
+          className="logo"
+        />
         <h2>Oak & Vine</h2>
       </div>
 
-      <div className="nav-links">
-        <a href="#home">Home</a>
-        <a href="#menu">Menu</a>
-        <a href="#about">About</a>
-        <a href="#reservation">Reservation</a>
-        <a href="#contact">Contact</a>
-      </div>
+      <div
+  className="hamburger"
+  onClick={() => setMenuOpen(!menuOpen)}
+>
+  {menuOpen ? "✕" : "☰"}
+</div>
+
+      <ul
+        className={
+          menuOpen
+            ? "nav-links active"
+            : "nav-links"
+        }
+      >
+        <li>
+          <a href="#home">Home</a>
+        </li>
+
+        <li>
+          <a href="#menu">Menu</a>
+        </li>
+
+        <li>
+          <a href="#about">About</a>
+        </li>
+
+        <li>
+          <a href="#reservation">Reservation</a>
+        </li>
+
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
     </nav>
   );
 }
